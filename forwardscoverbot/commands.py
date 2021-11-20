b2f7e4ac4af102a5a328bad0023ffee89de8a297
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-20 20:11:01 +0700

    Update commands.py

--- forwardscoverbot/commands.py
+++ forwardscoverbot/commands.py
@@ -1,20 +1,3 @@
-# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
-# Copyright (C) 2017-2020  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
-#
-# ForwardsCoverBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ForwardsCoverBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
-
-
 from forwardscoverbot import utils
 from forwardscoverbot import dbwrapper
 from forwardscoverbot import keyboards
@@ -53,21 +36,19 @@ def help_command(update, context):
 
 def disable_web_page_preview(update, context):
     if not update.message.reply_to_message:
-        text = ("This command permits to remove the web page preview from a message with "
-                "a link.\n\nUse it replying to the message the bot already echoed and you "
-                "want to disable the preview with this command.")
+        text = ("Perintah ini memungkinkan untuk menghapus pratinjau halaman web dari pesan dengan tautan.\n\nGunakan membalas pesan yang sudah digemakan bot dan Anda ingin menonaktifkan pratinjau dengan perintah ini.")
         update.message.reply_text(text=text)
         return
 
     if not update.message.reply_to_message.text:
-        text = "This message does not have a web page preview"
+        text = "Pesan ini tidak memiliki pratinjau halaman web"
         update.message.reply_to_message.reply_text(text=text, quote=True)
         return
 
     entities_list = [MessageEntity.URL, MessageEntity.TEXT_LINK]
     entities = update.message.reply_to_message.parse_entities(entities_list)
     if len(entities) == 0:
-        text = "This message does not have a web page preview"
+        text = "Pesan ini tidak memiliki pratinjau halaman web"
         update.message.reply_to_message.reply_text(text=text, quote=True)
         return
 
@@ -78,14 +59,13 @@ def disable_web_page_preview(update, context):
 def remove_caption(update, context):
     if not update.message.reply_to_message:
         text = (
-            "This command permits to remove caption from a message. Reply with this command to "
-            "the message where you want to remove the caption. Be sure the message has a caption."
+            "Perintah ini mengizinkan untuk menghapus caption dari sebuah pesan. Balas dengan perintah ini ke pijatan di mana Anda ingin menghapus caption. Pastikan pesan tersebut memiliki caption."
         )
         update.message.reply_text(text=text)
         return
 
     if not update.message.reply_to_message.caption:
-        text = "This message has no caption, so what should i remove? Use this command with messages having caption."
+        text = "Pesan ini tidak memiliki caption, jadi apa yang harus saya hapus? Gunakan perintah ini untuk pesan yang memiliki caption."
         context.bot.sendMessage(
             chat_id=update.message.from_user.id,
             text=text,
@@ -101,14 +81,13 @@ def remove_caption(update, context):
 def remove_buttons(update, context):
     if not update.message.reply_to_message:
         text = (
-            "This command permits to remove buttons from a message. Reply with this command to "
-            "the message where you want to remove the buttons. Be sure the message has buttons."
+             "Perintah ini memungkinkan untuk menghapus tombol dari pesan. Balas dengan perintah ini ke pijatan di mana Anda ingin menghapus tombol. Pastikan pesan memiliki tombol."
         )
         update.message.reply_text(text=text)
         return
 
     if not update.message.reply_to_message.reply_markup:
-        text = "This message has no buttons, so what should i remove? Use this command with messages having buttons."
+        text = "Pesan ini tidak memiliki tombol, jadi apa yang harus saya hapus? Gunakan perintah ini untuk pesan yang memiliki tombol."
         context.bot.sendMessage(
             chat_id=update.message.from_user.id,
             text=text,
@@ -124,13 +103,8 @@ def remove_buttons(update, context):
 def add_caption(update, context):
     if not update.message.reply_to_message:
         text = (
-            "<b>This command permits to add a caption to a message. Reply with this command and the caption after it to "
-            "the message where you want to add the caption.</b>\n\n<i>If the message already has a caption "
-            "this command will overwrite the current caption with the new one.\n"
-            "if the message doesn't support a caption, it simply won't add it, no errors are returned</i>\n\n\n"
-            "<i>Note: if the message is sent by you, you can just edit it to add the caption. This command is intended "
-            "in case for example you are fowarding from a channel a big file you don't want to download and "
-            "upload again.</i>"
+            "<b>Perintah ini mengizinkan untuk menambahkan caption ke pesan. Balas dengan perintah ini dan caption setelah pijatan di mana Anda ingin menambahkan caption.</b>\n\n<i>Jika pesan sudah memiliki caption, perintah ini akan menimpa caption saat ini dengan yang baru.\njika pesan tidak mendukung caption, tidak akan ditambahkan, tidak ada kesalahan yang dikembalikan</i>\n\n\n"
+            "<i>Catatan: jika pesan tersebut tidak sesuai dengan keinginan Anda, Anda dapat mengeditnya untuk menambahkan caption. Perintah ini ditujukan jika misalnya Anda meneruskan dari saluran file besar yang tidak ingin Anda unduh dan unggah lagi.</i>"
         )
         update.message.reply_text(text=text, parse_mode='HTML')
         return
@@ -139,7 +113,7 @@ def add_caption(update, context):
     caption_html = " ".join(update.message.text_html.split(" ")[1:])
 
     if len(caption) > t_consts.MAX_CAPTION_LENGTH:
-        text = "This caption is too long. max allowed: {} chars. Please retry removing {} chars.".format(
+        text = "Caption ini terlalu panjang. maksimum yang diizinkan: {} karakter. Harap coba lagi menghapus karakter {}.".format(
             t_consts.MAX_CAPTION_LENGTH,
             len(caption) - t_consts.MAX_CAPTION_LENGTH
         )
