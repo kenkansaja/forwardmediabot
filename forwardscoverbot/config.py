647e377b3b20748a4784e7778ceb49945e3fd2fd
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-20 17:44:12 +0700

    Update config.py

--- forwardscoverbot/config.py
+++ forwardscoverbot/config.py
@@ -1,39 +1,18 @@
-# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
-# Copyright (C) 2017-2019  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
-#
-# ForwardsCoverBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ForwardsCoverBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
+from dotenv import load_dotenv
+from os import path, getenv
 
 
-import yaml
-import sys
+if path.exists("local.env"):
+    load_dotenv("local.env")
+else:
+    load_dotenv()
 
 
-path = "config/config.yaml"
-if len(sys.argv) == 2:
-    path = sys.argv[1]
-try:
-    with open(path, 'r') as stream:
-        conf = yaml.load(stream, Loader=yaml.FullLoader)
-except FileNotFoundError:
-    print("\n\WARNING:\n"
-            "before of running forwardscoverbot you should create a file named `config.yaml`"
-            " in `config`.\n\nOpen `config/config.example.yaml`\ncopy all\ncreate a file "
-            "named `config.yaml`\nPaste and replace sample variables with true data."
-            "\nIf the file is in another path, you can specify it as the first parameter."
-            "\nExample: <forwardscoverbot /home/my_files/config.yaml>")
-    sys.exit()
+class Config:
+    BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
+    ADMINS = int(getenv("ADMINS", "1952053555"))
+    DB_PATH = getenv("DB_PATH", None)
 
-BOT_TOKEN = conf['bot_token']
-DB_PATH = conf['db_path']
-ADMINS = conf['admins']
+
+config = Config()
+
