39f991a72c2e02d392c908f5fbad6dd70b5f6035
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-20 20:57:27 +0700

    Update keyboards.py

--- forwardscoverbot/keyboards.py
+++ forwardscoverbot/keyboards.py
@@ -1,20 +1,3 @@
-# ForwardsCoverBot - don't let people on telegram forward with your name on the forward label
-# Copyright (C) 2017-2019  Dario <dariomsn@hotmail.it> (github.com/91DarioDev)
-#
-# ForwardsCoverBot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ForwardsCoverBot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with ForwardsCoverBot.  If not, see <http://www.gnu.org/licenses/>
-
-
 from forwardscoverbot.config import config
 from forwardscoverbot import constants
 
@@ -24,8 +7,9 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 def github_link_kb():
     button0 = InlineKeyboardButton(
             text="GROUP SUPPORT", 
-            url=config.GROUP), InlineKeyboardButton(text="CHANNEL", url=config.CHANNEL)
-    buttons_list = [[button0]]
+            url=config.GROUP)
+    button1 = InlineKeyboardButton(text="CHANNEL", url=config.CHANNEL)
+    buttons_list = [[button0], [button1]]
     keyboard = InlineKeyboardMarkup(buttons_list)
     return keyboard
 
