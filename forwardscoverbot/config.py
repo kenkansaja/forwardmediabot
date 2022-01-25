33c3d0f11780a31b04931d70e14a64cf887e7c37
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2022-01-26 01:44:21 +0700

    Update config.py

--- forwardscoverbot/config.py
+++ forwardscoverbot/config.py
@@ -12,8 +12,8 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
     ADMINS = int(getenv("ADMINS", None))
     DB_PATH = getenv("DB_PATH", None)
-    GROUP = getenv("GROUP", None)
-    CHANNEL = getenv("CHANNEL", None)
+    GROUP = getenv("GROUP", "t.me/musikkugroup")
+    CHANNEL = getenv("CHANNEL", "t.me/musikkuchannel")
     KODE = getenv("KODE", "https://github.com/kenkansaja/forwardmediabot")
 config = Config()
 
